f59fac8ffe80560e9999f83a978b7a0b7f4cfce8
Andrew Dass <dassandrew3@gmail.com>
2023-01-31 12:18:14 -0500

    Create testing_with_pytest.py

---
+++ Jenkins/Using_Jenkins_to_Observe_Pytest_Runs/testing_with_pytest.py
@@ -0,0 +1,12 @@
+import pytest
+import random
+
+def test_multiply_two_numbers(a, b, c):
+    expression = a * b
+    assert expression == c
+    
+a = random.randint(0, 3)
+b = random.randint(0, 3)
+c = random.randint(0, 9)
+    
+test_multiply_two_numbers(a, b, c)
