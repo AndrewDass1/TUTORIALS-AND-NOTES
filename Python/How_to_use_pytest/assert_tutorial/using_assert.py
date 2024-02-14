c384353a7867cc3a1260030f9ca25b46b28fa876
Andrew Dass <dassandrew3@gmail.com>
2024-02-14 15:06:03 -0500

    Create using_assert.py
    The assert keyword will see if a statement is true. If true, nothing is executed in the Terminal. If false, then an error appears in the Terminal. You cannot enclose "assert" with a "print()" statement.

---
+++ Python/How_to_use_pytest/assert_tutorial/using_assert.py
@@ -0,0 +1,6 @@
+x = input("Enter a word: ")
+
+# The assert keyword will see if a statement is true. If true, nothing is executed in the Terminal. If false, then an error appears in the Terminal. You cannot enclose "assert" with
+# a "print()" statement.
+
+assert x == "Hello World"
