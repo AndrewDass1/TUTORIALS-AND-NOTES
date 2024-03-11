621189161969cc0a6822ae8ebcc355a3969bc91a
Andrew Dass <dassandrew3@gmail.com>
2024-03-11 12:46:02 -0400

    Create javascript_file.js
    This is the JavaScript code that declared comments, a variable and a function.

---
+++ JavaScript/Node.js/Run JS Code with Node.js/javascript_file.js
@@ -0,0 +1,16 @@
+// Write a comment starting with two "//" and then write a sentence or comment afterward
+
+// Make a Variable
+var variable_1 = 3;
+
+// Output a variable with console.log. In the terminal run "node" and the file name of the javascript file.
+console.log(variable_1);
+
+// Writing a Function
+
+function name_of_function() {
+    return "Hello";
+}
+
+// Printing it with console.log
+console.log(name_of_function())
