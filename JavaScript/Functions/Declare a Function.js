0e6c9ff35368aba7348b3145ef83db33ce94409c
Andrew Dass <dassandrew3@gmail.com>
2024-03-19 17:26:26 -0400

    Create Declare a Function.js

---
+++ JavaScript/Functions/Declare a Function.js
@@ -0,0 +1,9 @@
+function insert_Function_Name(num1, num2, num3) {
+    return num1 * num2 * num3;
+}
+
+let multiplication = insert_Function_Name(3, 4, 5);
+
+let print_function_result = multiplication;
+
+console.log(print_function_result);
