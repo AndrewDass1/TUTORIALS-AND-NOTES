7fedf1af0dc506beb840f3c10b0a361be23d7d9f
Andrew Dass <dassandrew3@gmail.com>
2025-02-24 11:31:51 -0500

    Update connect_python_to_sql.py
    Added code to show the databases after it creates the database.

--- Python/SQL/Integrating Python and SQL together/connect_python_to_sql.py
+++ Python/SQL/Integrating Python and SQL together/connect_python_to_sql.py
@@ -1,7 +1,7 @@
 # 1st import the module
 import mysql.connector
 
-# 2nd: connect to database, both lines of code down below is necessary
+# 2nd: connect to database, both lines of code down below is essential
 
 cnx = mysql.connector.connect(user='...', host='127.0.0.1', password='...')
 
@@ -10,5 +10,11 @@ cursor = cnx.cursor()
 # 3rd: write sql code: 
 make_database = "create database name_of_database"
 
-# 4th: execute sql command
+# 4th: execute
 cursor.execute(make_database)
+
+#5th: show changes or databases
+list_of_databases = cursor.execute("show databases")
+
+for i in cursor:
+    print(i)
